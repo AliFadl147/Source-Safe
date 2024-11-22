eabe3e48a501a18d04729aa77bf1e5e7e6d6282f
AliFadl147 <alihilalfadl147@gmail.com>
2024-11-23 00:04:57 +0300

    feature-adding-dashboard-desktop-layout-branch

---
+++ lib/Features/dashboard/presentation/views/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:source_safe_project/Features/dashboard/presentation/views/widgets/custom_drawer.dart';
+
+class DashboardDesktopLayout extends StatelessWidget {
+  const DashboardDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          flex: 3,
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 40),
+                          child: SizedBox()//AllExpensessAndQuickInvoiceSection(),
+                        )),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Expanded(
+                        child: Column(
+                      children: [
+                        SizedBox(
+                          height: 40,
+                        ),
+                        SizedBox(),//MyCardsAndTransctionHistorySection(),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        Expanded(child: SizedBox()/*IncomeSection()*/),
+                      ],
+                    )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color kPrimaryColor = const Color(0xff5BA2E0);
   static Color pastelBlue = const Color(0xFF99C7F5);
+  static Color darkShadeOfCyanBlue = const Color(0xFF064061);
   static Color white = const Color(0xffFFFFFF);
   static Color black = const Color(0xff000000);
-  static Color ligthBlack = const Color(0xff212121);
+  //static Color ligthBlack = const Color(0xff212121);
   static Color deepCharcoal = const Color(0xff2F2E41);
   static Color sonicSilver = const Color(0xff757575);
   static Color gray74 = const Color(0xffBDBDBD);
   static Color guyabano = const Color(0xFFF8F8F8);
   static Color gainsboro = const Color(0xffF5F5F5);
   static Color red = const Color(0xffF44336);
-  static Color darkGainsboro = const Color(0xff9B9B9B);
+  //static Color darkGainsboro = const Color(0xff9B9B9B);
   static Color lightGray = const Color(0xffE0E0E0); // 9B9B9B
+  static Color mediumGray = const Color(0xFFAAAAAA);
   static Color successGren = const Color(0xff4BB543);
 }
